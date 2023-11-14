978763a484912734735febe91aa81e6d7b98dfd7
Alexander Salamatov <ch1seL@ya.ru>
2023-11-14 21:37:06 +0300

    fix base path and repo name

--- src/Shared/Constants.cs
+++ src/Shared/Constants.cs
@@ -1,6 +1,6 @@
 namespace Shared;
 
 public static class Constants {
-    private const string GitHubRepo = "capybara-pro-code/github-hosted-app";
+    private const string GitHubRepo = "everscale-actions/inshallah-bot";
     public const string TunnelUrl = $"https://raw.githubusercontent.com/{GitHubRepo}/tunnel-url/tunnel-url.txt";
 }
