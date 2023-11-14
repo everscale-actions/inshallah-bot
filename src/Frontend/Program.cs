7d2bc58155cdf951c5fb6e649732859efaa2727b
Aleksander Salamatov <15122233+ch1seL@users.noreply.github.com>
2023-11-14 21:31:41 +0300

    Initial commit

---
+++ src/Frontend/Program.cs
@@ -0,0 +1,16 @@
+using Frontend;
+using Frontend.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<RenewBackendUrlHandler>();
+builder.Services.AddScoped<BackendService>();
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var app = builder.Build();
+
+await app.RunAsync();
